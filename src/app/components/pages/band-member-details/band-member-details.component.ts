81f47cad2fc4b1c62984cdaf32f64a709139e994
Paulo Henrique <ph.1928@hotmail.com>
2024-08-16 10:59:22 -0300

    Cria estrutura básica de componentes

---
+++ src/app/components/pages/band-member-details/band-member-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-band-member-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './band-member-details.component.html',
+  styleUrl: './band-member-details.component.css'
+})
+export class BandMemberDetailsComponent {
+
+}
---
+++ src/app/components/pages/discography/discography.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-discography',
+  standalone: true,
+  imports: [],
+  templateUrl: './discography.component.html',
+  styleUrl: './discography.component.css'
+})
+export class DiscographyComponent {
+
+}
