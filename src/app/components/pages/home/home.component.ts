c0ca62504e4a9c64e4f88aced766438222676c22
Paulo Henrique <ph.1928@hotmail.com>
2024-08-18 22:29:52 -0300

    WIP: formulário newsletter

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,12 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  isSubscribed: boolean = false;
 
+  onSubmit(): void {
+    this.isSubscribed = true;
+  }
 }
