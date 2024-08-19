876d732d72a7b1966d84a9ab3e2af7f79d249a33
Paulo Henrique <ph.1928@hotmail.com>
2024-08-19 11:18:10 -0300

    WIP: formulário newsletter

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Subscription } from '../../../models/subscription';
 
 @Component({
   selector: 'app-home',
@@ -9,10 +10,19 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
-  isSubscribed: boolean = false;
+export class HomeComponent{
+  isFormVisible: boolean = true;
+  isConfirmationVisible: boolean = false;
+  subscription: Subscription = {
+    name: '',
+    email: ''
+  }
 
   onSubmit(): void {
-    this.isSubscribed = true;
+    this.isConfirmationVisible = true;
+  }
+
+  onClose(): void {
+    this.isFormVisible = false;
   }
 }
